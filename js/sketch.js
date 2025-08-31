c809df4981884419e926b4b50c944d3e5b3795a5
ruduba <vatbudur@gmail.com>
2025-08-31 14:27:03 +0530

    renders static test image into ASCII characters

--- js/sketch.js
+++ js/sketch.js
@@ -1,23 +1,42 @@
-function make2Darray(){
-  let arr = new Array(cols);
-  for(let i =0; i<arr.length; i++){
-  arr[i] = new Array(rows);
-  }
-return arr;
+const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 
+
+function preload(){
+    test = loadImage('assets/test.png');
 }
 
-let grid;
-let cols =10;
-let rows = 10;
 
 function setup(){
-  
-  grid = make2Darray(10, 10);
-  for(let i=0; i<cols; i++){
-    for(let j=0; j<rows; j++){
-    grid[i][j] = random(2);
+    createCanvas(400, 400).parent("canvas-container");
+}
+
+function draw(){
+    background(0);
+    //image(test, 0, 0, width, height);
+
+    let w = width/test.width;
+    let h = height/test.height;
+
+    test.loadPixels();
+
+    for(let i =0; i< test.width; i++){
+        for(let j =0; j< test.height; j++){
+            const pIndex = (i+j*test.width) * 4;
+            const r = test.pixels[pIndex+0];
+            const g = test.pixels[pIndex+1];
+            const b = test.pixels[pIndex+2];
+            const avg = (r+ g+ b)/3;
+
+            noStroke();
+            fill(255);
+            //square(i*w, j*h, w);
+
+            const len = density.length;
+            const cIndex = floor(map(avg, 0, 255, len, 0));
+            textSize(w);
+            textAlign(CENTER, CENTER);
+            text(density.charAt(cIndex), i*w+w*0.5, j*h+h*0.5);
+        }
     }
-  }
 
 }
